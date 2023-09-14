d927fb7551411b2577880067b908f078f6023523
Kuda Mbata <kudambata2005@gmail.com>
2023-09-14 17:32:07 +0200

    Fixed middleware again

--- db/settings.py
+++ db/settings.py
@@ -63,7 +63,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
